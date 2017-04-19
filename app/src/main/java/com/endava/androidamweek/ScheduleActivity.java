f33d613ed449b7600fbd4fc7f780084ac4e60024
Alexandr Persin <apersin@endava.com>
2017-04-19 16:08:37 +0300

    Setup firebase

--- app/src/main/java/com/endava/androidamweek/ScheduleActivity.java
+++ app/src/main/java/com/endava/androidamweek/ScheduleActivity.java
@@ -3,7 +3,12 @@ package com.endava.androidamweek;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ScheduleActivity extends AppCompatActivity {
+
+    DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
