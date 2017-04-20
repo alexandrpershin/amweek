de55ff918d54e521dd6aea1d0e400d3f0614dfc2
vplacinta <vladimir.placinta@endava.com>
2017-04-20 15:17:27 +0300

    feature/IIA-837 merged

--- app/src/main/java/com/endava/androidamweek/ScheduleActivity.java
+++ app/src/main/java/com/endava/androidamweek/ScheduleActivity.java
@@ -1,18 +1,56 @@
 package com.endava.androidamweek;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
-public class ScheduleActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
+public class ScheduleActivity extends AppCompatActivity implements OnTabSelectListener {
+
+    @BindView(R.id.bottomBar)
+    BottomBar bottomBar;
+
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        ButterKnife.bind(this);
+        bottomBar.setOnTabSelectListener(this);
+
+    }
+
+    private void replaceFragment() {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragmentFrame, new ScheduleFragment())
+                .commit();
+
+    }
+
+    @Override
+    public void onTabSelected(@IdRes int tabId) {
+        switch (tabId) {
+            case R.id.monday:
+                break;
+            case R.id.tuesday:
+                break;
+            case R.id.wednesday:
+                break;
+            case R.id.thursday:
+                break;
+            case R.id.friday:
+                break;
+            default:
+                break;
+        }
     }
 }
