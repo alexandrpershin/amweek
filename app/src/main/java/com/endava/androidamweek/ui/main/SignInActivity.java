1bae18b3aec12a4b0594147f230568a5b65158c7
eparpalac <elviraparpalac@gmail.com>
2017-05-22 14:57:57 +0300

    Fixed schedule layout

--- app/src/main/java/com/endava/androidamweek/ui/main/SignInActivity.java
+++ app/src/main/java/com/endava/androidamweek/ui/main/SignInActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
